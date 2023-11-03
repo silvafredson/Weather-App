f8e2c4d7738189b40aaca4c06bd50f527dbbe6ed
Fredson Silva <fredson.silva@inchurch.com.br>
2023-11-03 20:59:22 -0300

    Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Fredson Silva on 03/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
